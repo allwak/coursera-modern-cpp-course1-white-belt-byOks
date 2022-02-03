236d912137ec64f77a1361182d220ab2469596bd
allwak <oksamytny@phystech.edu>
2022-02-03 01:11:40 +0000

    Update Week - 1

--- Week - 1/[Part 3] Conditional operator and loops/Lessons/Lesson_1.cpp
+++ Week - 1/[Part 3] Conditional operator and loops/Lessons/Lesson_1.cpp
@@ -1,43 +1,61 @@
 #include <iostream>
-#include <cstdio>
 #include <string>
 #include <vector>
 #include <map>
 using namespace std;
 
-struct Person {
-    string name;
-    string surname;
-    int age;
-};
-
 int main() {
+    int a = 3;
+    int b = 5;
+    if (a < b) {
+        cout << "a is less than b" << endl; // a is less than b
+    }
 
-    int x = -5;
-    double pi = 3.14;
-    bool logical_value = false;
-    char symbol = 'Z';
-    
-    string hw = "Hello, World!";
+    string string1 = "fire";
+    string string2 = "water";
+    string string3 = "fog";
 
-    vector<int> nums = {1, 3, 5, 7};
-    cout << nums.size() << "\n";
-    // 4
+    if (string1 < string2) {
+        cout << "string1 is less than string2" << endl; // string1 is less than string2
+    }
+    if (string1 == string3) {
+        cout << "string1 is equal to string3" << endl; 
+    } else {
+        cout << "string1 is not equal to string3" << endl; 
+    }
+    // string1 is not equal to string3
 
-    map<string, int> name_to_value;
-    name_to_value["one"] = 1;
-    name_to_value["two"] = 2;
+    vector<string> strings_vec_1 = {"fire", "water"};
+    vector<string> strings_vec_2 = {"fog", "air"};
 
-    cout << "two is " << name_to_value["two"] << "\n";
-    // two is 2
+    if (strings_vec_1 == strings_vec_2) {
+        cout << "strings_vec_1 is equal to strings_vec_2" << endl; 
+    } else {
+        cout << "strings_vec_1 is not equal to strings_vec_2" << endl; 
+    }
+    // strings_vec_1 is not equal to strings_vec_2
 
-    vector<Person> staff; 
-    staff.push_back({"Ivan", "Ivanov", 25});
-    staff.push_back({"Petr", "Petrov", 32});
+    map<string, int> dict1;
+    dict1["one"] = 1;
+    map<string,int> dict2;
 
-    cout << staff[0].name << "\n";
-    // Ivan
+    if (dict1 == dict2) {
+        cout << "dict1 is equal to dict2" << endl; 
+    } else {
+        cout << "dict1 is not equal to dict2" << endl; 
+    }
+    // dict1 is not equal to dict2
+
+    string s = "abc";
+    if (!s.empty() && s < "zzz") {
+        cout << s << endl;
+    }
+    // abc
+
+    if (!s.empty() || s < "zzz") {
+        cout << s << endl;
+    }
+    // abc
 
     return 0;
 }
-
